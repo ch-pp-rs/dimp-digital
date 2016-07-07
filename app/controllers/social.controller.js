c9994e44412fc64c64ebeb1ce07e188e7db6ca65
Ben Chaplin <ben.chaplin@gravitysupplychain.com>
2016-07-07 16:26:14 +0100

    Remove console.log.

--- app/controllers/social.controller.js
+++ app/controllers/social.controller.js
@@ -2,7 +2,6 @@
 
 angular.module('dimpApp').controller('SocialController', function ($scope, $http) {
   $http.get('https://dimp-api.herokuapp.com/api/twitter').then(function (response) {
-    console.log(response.data[5]);
     $scope.posts = response.data;
   });
 });
